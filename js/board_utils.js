7f306e2d14082f67d6836c62c60c75658b55c19b
Sunit Shrestha <sunitshrsth@gmail.com>
2023-04-20 11:34:23 +0545

    Basic Moves added.

--- js/board_utils.js
+++ js/board_utils.js
@@ -1,7 +1,7 @@
 import * as moveset from "./moveset.js";
-export { moves, add, remove, addListeners };
+export { addMoves, add, remove, addListeners, isEmpty, isTeam, isOpponent };
 
-function moves(e) {
+function addMoves(e) {
 	let box = e.currentTarget;
 	moveset[box.piece[1]](box.piece[0], box.id);
 }
@@ -23,15 +23,49 @@ function addListeners(color) {
 	for (let i = 0; i < 8; i++) {
 		for (let j = 0; j < 8; j++) {
 			let box = document.getElementById(i * 10 + j);
-			box.removeEventListener("click", moves);
+			box.removeEventListener("click", addMoves);
 		}
 	}
 	for (let i = 0; i < 8; i++) {
 		for (let j = 0; j < 8; j++) {
 			let box = document.getElementById(i * 10 + j);
 			if (box.piece[0] == color) {
-				box.addEventListener("click", moves);
+				box.addEventListener("click", addMoves);
 			}
 		}
+	}
+}
+
+function isEmpty(pos) {
+	let elem = document.getElementById(pos);
+	if (elem == null) {
+		return false;
+	}
+	if (elem.classList.length > 1) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function isTeam(pos, color) {
+	let elem = document.getElementById(pos);
+	if (elem == null) {
+		return false;
+	} else if (elem.classList.length == 1) {
+		return false;
+	} else {
+		return elem.classList[1][0] == color;
+	}
+}
+
+function isOpponent(pos, color) {
+	let elem = document.getElementById(pos);
+	if (elem == null) {
+		return false;
+	} else if (elem.classList.length == 1) {
+		return false;
+	} else {
+		return !(elem.classList[1][0] == color);
 	}
 }
--- js/moveset.js
+++ js/moveset.js
@@ -7,8 +7,14 @@ q -> queen,
 k -> king
 */
 
-import { add, remove, addListeners } from "./board_utils.js";
-export { p, b, r, q, k };
+import {
+	add,
+	remove,
+	addListeners,
+	isEmpty,
+	isOpponent,
+} from "./board_utils.js";
+export { p, n, b, r, q, k };
 
 function move(e) {
 	let hint = e.currentTarget;
@@ -18,6 +24,15 @@ function move(e) {
 	removeHints();
 	let nextTurn = piece[0] == "w" ? "b" : "w";
 	addListeners(nextTurn);
+}
+
+function showHints(hints, hintFor) {
+	for (let i = 0; i < hints.length; i++) {
+		let hint = document.getElementById(hints[i]);
+		hint.classList.add("hint");
+		hint.hintFor = hintFor;
+		hint.addEventListener("click", move);
+	}
 }
 
 function removeHints() {
@@ -31,35 +46,126 @@ function removeHints() {
 	}
 }
 
-function p(color, pos) {
+function p(color, posString) {
 	removeHints();
 	let hints = [];
+	let pos = Number(posString);
 	if (color == "w") {
-		hints.push(Number(pos) - 10);
-		if (Number(pos[0]) == 6) {
-			hints.push(Number(pos) - 20);
+		if (isEmpty(pos - 10)) {
+			hints.push(pos - 10);
+			if (posString[0] == 6 && isEmpty(pos - 20)) {
+				hints.push(pos - 20);
+			}
+		}
+		if (!isEmpty(pos - 11) && isOpponent(pos - 11, color)) {
+			hints.push(pos - 11);
+		}
+		if (!isEmpty(pos - 9) && isOpponent(pos - 9, color)) {
+			hints.push(pos - 9);
 		}
 	} else {
-		hints.push(Number(pos) + 10);
-		if (Number(pos[0]) == 1) {
-			hints.push(Number(pos) + 20);
+		if (isEmpty(pos + 10)) {
+			hints.push(pos + 10);
+			if (posString[0] == 1 && isEmpty(pos + 20)) {
+				hints.push(pos + 20);
+			}
+		}
+		if (!isEmpty(pos + 11) && isOpponent(pos + 11, color)) {
+			hints.push(pos + 11);
+		}
+		if (!isEmpty(pos + 9) && isOpponent(pos + 9, color)) {
+			hints.push(pos + 9);
+		}
+	}
+	showHints(hints, pos);
+}
+
+function n(color, posString) {
+	let hints = [];
+	let jumps = [-8, -12, -19, -21, 8, 12, 19, 21];
+	let pos = Number(posString);
+	for (let i = 0; i < jumps.length; i++) {
+		let hintPos = pos + jumps[i];
+		if (isEmpty(hintPos) || isOpponent(hintPos, color)) {
+			hints.push(hintPos);
+		}
+	}
+	showHints(hints, pos);
+}
+
+function b(color, posString) {
+	let hints = [];
+	let jumps = [-9, -11, 9, 11];
+	let pos = Number(posString);
+	for (let i = 0; i < jumps.length; i++) {
+		let hintPos = pos;
+		while (true) {
+			hintPos = hintPos + jumps[i];
+			if (isEmpty(hintPos)) {
+				hints.push(hintPos);
+			} else if (isOpponent(hintPos, color)) {
+				hints.push(hintPos);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+	showHints(hints, pos);
+}
+
+function r(color, posString) {
+	let hints = [];
+	let jumps = [-1, -10, 1, 10];
+	let pos = Number(posString);
+	for (let i = 0; i < jumps.length; i++) {
+		let hintPos = pos;
+		while (true) {
+			hintPos = hintPos + jumps[i];
+			if (isEmpty(hintPos)) {
+				hints.push(hintPos);
+			} else if (isOpponent(hintPos, color)) {
+				hints.push(hintPos);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+	showHints(hints, pos);
+}
+
+function q(color, posString) {
+	let hints = [];
+	let jumps = [-1, -9, -10, -11, 1, 9, 10, 11];
+	let pos = Number(posString);
+	for (let i = 0; i < jumps.length; i++) {
+		let hintPos = pos;
+		while (true) {
+			hintPos = hintPos + jumps[i];
+			if (isEmpty(hintPos)) {
+				hints.push(hintPos);
+			} else if (isOpponent(hintPos, color)) {
+				hints.push(hintPos);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+	showHints(hints, pos);
+}
+
+function k(color, posString) {
+	let hints = [];
+	let jumps = [-1, -9, -10, -11, 1, 9, 10, 11];
+	let pos = Number(posString);
+	for (let i = 0; i < jumps.length; i++) {
+		let hintPos = pos + jumps[i];
+		if (isEmpty(hintPos) || isOpponent(hintPos, color)) {
+			hints.push(hintPos);
 		}
 	}
 	console.log(hints);
-	for (let i = 0; i < hints.length; i++) {
-		let hint = document.getElementById(hints[i]);
-		hint.classList.add("hint");
-		hint.hintFor = pos;
-		hint.addEventListener("click", move);
-	}
+	showHints(hints, pos);
 }
-
-function n(color, pos) {}
-
-function b(color, pos) {}
-
-function r(color, pos) {}
-
-function q(color, pos) {}
-
-function k(color, pos) {}
